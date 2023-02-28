9c5b868b825bc177176645e640c36c885bc3020b
getset.01@sequor.com.br <getset.01@sequor.com.br>
2023-02-28 09:55:16 -0300

    Criando menu lateral

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -1,5 +1,5 @@
 
-import { Drawer, useTheme,  Avatar, Divider} from '@mui/material'
+import { Drawer, useTheme,  Avatar, Divider, List, ListItemButton, Icon, ListItemIcon, ListItemText} from '@mui/material'
 import { Box } from '@mui/system'
 
 
@@ -14,9 +14,9 @@ export const MenuLateral : React.FC <IMenuLateralProps>= ({children}) =>{
     return(
       <>
          <Drawer variant='permanent'>
-            <Box width={theme.spacing(28)} display='flex'>
+            <Box width={theme.spacing(28)} height='100%'display='flex' flexDirection={'column'}>
 
-                <Box width='100%' height={theme.spacing(20)} display='flex' alignItems='center' justifyContent='center'>
+               <Box width='100%' height={theme.spacing(20)} display='flex' alignItems='center' justifyContent='center'>
                     <Avatar 
                         sx={{height: theme.spacing(12), width: theme.spacing(12) }}
                         src="/static/images/avatar/1.jpg" 
@@ -26,14 +26,19 @@ export const MenuLateral : React.FC <IMenuLateralProps>= ({children}) =>{
                <Divider/>
 
                <Box flex={1}>
-                 
-                 
+                   <List component="nav">
+                     <ListItemButton>
+                        <ListItemIcon>
+                            <Icon>home</Icon>
+                        </ListItemIcon>
+                        <ListItemText primary='Página inicial'/>
+                     </ListItemButton>
+                   </List>
                </Box>
            </Box>
          </Drawer>
-    
 
-        <Box height='100100vh' marginLeft={theme.spacing(28)}> 
+        <Box height='100vh' marginLeft={theme.spacing(28)}> 
            {children}
         </Box>
       </>
